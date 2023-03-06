9431ca38d56c5b9a56280be3601a334e98a85cdc
Cajemam Carente <106678041+cajemam@users.noreply.github.com>
2023-03-06 18:53:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def proxy():
+  url = request.headers.get('url')
+  if (url is None):
+    return ('No URL specified in headers', 400)
+  try:
+    data = request.get_data()
+    headers = {
+      'Content-type': request.headers.get('Content-Type'),
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    }
+    response = request.post(url, data=data, headers=headers)
+    return (response.content, response.status_code, headers)
+  except requests.exceptions.RequestException as e:
+    return str(e), 500
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
